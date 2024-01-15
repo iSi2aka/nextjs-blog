c3b3f90837b9f104685716955e3070466652f2fd
iSi2aka <n.ishizaka@outlook.com>
2024-01-15 22:02:59 +0900

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-
+ 
 export default function Home() {
   return (
     <div className={styles.container}>
